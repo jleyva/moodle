ed80bb852f55d9f44ef906c4bcb4d3c529ef4cb3
Juan Leyva <juanleyvadelgado@gmail.com>
2017-02-06 13:20:19 +0100

    MDL-57823 mod_feedback: Implement the check_updates callback for mod_feedback

--- mod/feedback/lib.php
+++ mod/feedback/lib.php
@@ -3320,3 +3320,38 @@ function feedback_can_view_analysis($feedback, $context, $courseid = false) {
 
     return feedback_is_already_submitted($feedback->id, $courseid);
 }
+
+/**
+ * Check if the module has any update that affects the current user since a given time.
+ *
+ * @param  cm_info $cm course module data
+ * @param  int $from the time to check updates from
+ * @param  array $filter if we need to check only specific updates
+ * @return stdClass an object with the different type of areas indicating if they were updated or not
+ * @since Moodle 3.2
+ */
+function feedback_check_updates_since(cm_info $cm, $from, $filter = array()) {
+    global $DB, $USER, $CFG;
+    require_once($CFG->dirroot . '/mod/feedback/locallib.php');
+
+    $updates = course_check_module_updates_since($cm, $from, array(), $filter);
+
+    // Check for new submissions.
+    $updates->submissionscompleted = (object) array('updated' => false);
+    $updates->submissionsuncompleted = (object) array('updated' => false);
+    $select = 'feedback = ? AND userid = ? AND timemodified > ?';
+    $params = array($cm->instance, $USER->id, $from);
+
+    $submissionscompleted = $DB->get_records_select('feedback_completed', $select, $params, '', 'id');
+    if (!empty($submissionscompleted)) {
+        $updates->submissionscompleted->updated = true;
+        $updates->submissionscompleted->itemids = array_keys($submissionscompleted);
+    }
+    $submissionsuncompleted = $DB->get_records_select('feedback_completedtmp', $select, $params, '', 'id');
+    if (!empty($submissionsuncompleted)) {
+        $updates->submissionsuncompleted->updated = true;
+        $updates->submissionsuncompleted->itemids = array_keys($submissionsuncompleted);
+    }
+
+    return $updates;
+}
