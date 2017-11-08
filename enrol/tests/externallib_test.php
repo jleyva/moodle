a4f3d3a7d56073b5e7bf8d4256ed099bfa9c1bd0
Juan Leyva <juanleyvadelgado@gmail.com>
2017-11-08 15:35:33 +0100

    MDL-60738 webservice: Clean theme and lang properly
    Sometimes the "theme" and "lang" fields in the user and course tables
    in the database are set to incorrect values (uninstalled or
    non-existent themes and language packs).
    This makes Web Services functions to fail because the WS server
    validate the returned data using the validate_param function that clean
    parameters.

--- enrol/tests/externallib_test.php
+++ enrol/tests/externallib_test.php
@@ -373,8 +373,12 @@ class core_enrol_externallib_testcase extends externallib_advanced_testcase {
             'showgrades'       => true
         );
 
+        $coursedata2 = array(
+            'lang'             => 'kk', // Check invalid language pack.
+        );
+
         $course1 = self::getDataGenerator()->create_course($coursedata1);
-        $course2 = self::getDataGenerator()->create_course();
+        $course2 = self::getDataGenerator()->create_course($coursedata2);
         $courses = array($course1, $course2);
 
         // Enrol $USER in the courses.
@@ -411,6 +415,9 @@ class core_enrol_externallib_testcase extends externallib_advanced_testcase {
                 foreach ($coursedata1 as $fieldname => $value) {
                     $this->assertEquals($courseenrol[$fieldname], $course1->$fieldname);
                 }
+            } else {
+                // Check language pack. Should be empty since an incorrect one was used when creating the course.
+                $this->assertEmpty($courseenrol['lang']);
             }
         }
     }
